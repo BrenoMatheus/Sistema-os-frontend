4b5e32b0a963ff78c4aa4bc8790ebd1128083b02
BrenoMatheus <breno.moj99@gmail.com>
2024-02-04 16:00:44 -0300

    fix: add instruction for login

--- src/shared/components/login/Login.tsx
+++ src/shared/components/login/Login.tsx
@@ -1,9 +1,17 @@
 import { useState } from 'react';
-import { Box, Button, Card, CardActions, CardContent, CircularProgress, TextField, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CircularProgress,
+  TextField,
+  Typography,
+} from '@mui/material';
 import * as yup from 'yup';
 
 import { useAuthContext } from '../../contexts';
-
 
 const loginSchema = yup.object().shape({
   email: yup.string().email().required(),
@@ -23,22 +31,20 @@ export const Login: React.FC<ILoginProps> = ({ children }) => {
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
 
-
   const handleSubmit = () => {
     setIsLoading(true);
 
     loginSchema
       .validate({ email, password }, { abortEarly: false })
-      .then(dadosValidados => {
-        login(dadosValidados.email, dadosValidados.password)
-          .then(() => {
-            setIsLoading(false);
-          });
+      .then((dadosValidados) => {
+        login(dadosValidados.email, dadosValidados.password).then(() => {
+          setIsLoading(false);
+        });
       })
       .catch((errors: yup.ValidationError) => {
         setIsLoading(false);
 
-        errors.inner.forEach(error => {
+        errors.inner.forEach((error) => {
           if (error.path === 'email') {
             setEmailError(error.message);
           } else if (error.path === 'password') {
@@ -48,56 +54,76 @@ export const Login: React.FC<ILoginProps> = ({ children }) => {
       });
   };
 
-
-  if (isAuthenticated) return (
-    <>{children}</>
-  );
+  if (isAuthenticated) return <>{children}</>;
 
   return (
-    <Box width='100vw' height='100vh' display='flex' alignItems='center' justifyContent='center'>
-
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
       <Card>
         <CardContent>
-          <Box display='flex' flexDirection='column' gap={2} width={250}>
-            <Typography variant='h6' align='center'>Identifique-se</Typography>
+          <Box display="flex" flexDirection="column" gap={2} width={250}>
+            <Typography variant="h6" align="center">
+              Identifique-se
+            </Typography>
 
             <TextField
               fullWidth
-              type='email'
-              label='Email'
+              type="email"
+              label="Email"
+              defaultValue={'matheus@gmail.com'}
               value={email}
               disabled={isLoading}
               error={!!emailError}
               helperText={emailError}
               onKeyDown={() => setEmailError('')}
-              onChange={e => setEmail(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
             />
 
             <TextField
               fullWidth
-              label='Senha'
-              type='password'
+              label="Senha"
+              type="password"
+              defaultValue={'1234567'}
               value={password}
               disabled={isLoading}
               error={!!passwordError}
               helperText={passwordError}
               onKeyDown={() => setPasswordError('')}
-              onChange={e => setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </Box>
+          <Typography
+            width="20vw"
+            variant="body2"
+            color={'GrayText'}
+          >
+            email: matheus@gmail.com 
+            <br />senha: 1234567
+          </Typography>
         </CardContent>
         <CardActions>
-          <Box width='100%' display='flex' justifyContent='center'>
-
+          <Box width="100%" display="flex" justifyContent="center">
             <Button
-              variant='contained'
+              variant="contained"
               disabled={isLoading}
               onClick={handleSubmit}
-              endIcon={isLoading ? <CircularProgress variant='indeterminate' color='inherit' size={20} /> : undefined}
+              endIcon={
+                isLoading ? (
+                  <CircularProgress
+                    variant="indeterminate"
+                    color="inherit"
+                    size={20}
+                  />
+                ) : undefined
+              }
             >
               Entrar
             </Button>
-
           </Box>
         </CardActions>
       </Card>
